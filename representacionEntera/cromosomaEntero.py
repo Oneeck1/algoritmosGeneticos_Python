4a049679ba1d92a6d3d474944964a95d02005e17
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-22 14:34:12 -0600

    segundo ejercicio

---
+++ representacionEntera/cromosomaEntero.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 14:28:16 2021
+
+UNIVERSIDAD AUTÓNOMA DEL ESTADO DE MÉXICO
+CENTRO UNIVERSITARIO UAEM ZUMPANGO
+INGENIERÍA EN COMPUTACIÓN
+ALGORITMOS GENÉTICOS
+Alumno: Gustavo Rodrígez Calzada
+Profesor: Asdrúbal Lopez Chau
+
+Descripción: Implementa un cromosoma para representar numeros
+enteros
+
+
+
+@author: gustavo
+"""
+
+
+
+class Entero:
+    
+    def __init__(self):
+        pass
+    def inicializa(self, min=0, max=32):
+        pass
+    def mutar(self):
+        pass
+    def cruzar(self, otro):
+        pass
