fa3778338ff3e085943d94aa80914088506c5211
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-12 20:04:37 -0600

    creando archivo para la representacion de soluciones numericas

--- representacionReal/CromosomaReal.py
+++ representacionReal/CromosomaReal.py
@@ -3,6 +3,16 @@
 """
 Created on Mon Mar  8 15:11:26 2021
 
+CENTRO UNIVERSITARIO UAEM ZUMPANGO
+INGENIERÍA EN COMPUTACIÓN
+ALGORITMOS GENÉTICOS
+Alumno: Gustavo Rodrígez Calzada
+Profesor: Asdrúbal Lopez Chau
+
+Descripción: Implementa un cromosoma real para representar numeros
+enteros
+
+
 @author: gustavo
 """
 
