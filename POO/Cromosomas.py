ba2876418e6f8f2a56a0d89cbe69231ef9170d01
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-08 23:09:39 -0500

    representacion real AG

--- POO/Cromosomas.py
+++ POO/Cromosomas.py
@@ -219,7 +219,7 @@ class GenReal(GenNum):
         self.gray = gray
         # Calculo del número mínimo de bits para representar un valor entre
         # vMin y vMax
-        v = max([np.abs(vMin), np.abs(vMax)])  
+        v = max([vMin, vMax])  
         
         self.nbits = int(np.ceil(np.log(v + 1)/np.log(2)) + 1) # Saber cuantos bits ocupo para parte entera
         self.cromosoma = random.choices([0, 1], k = self.nbits)
