e0d4e14b5e33d26c83cbded593434dafe9111783
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-19 15:11:02 -0500

    sobrante estocastico

--- Tenicas_Seleccion/Sobrante_Estocastico.py
+++ Tenicas_Seleccion/Sobrante_Estocastico.py
@@ -8,6 +8,7 @@ Created on Mon Apr 19 14:31:10 2021
 
 import random 
 import numpy as np
+import math
 # OBJEIVO: CAFE
 
 def fitnessFuncion(individuo):
@@ -31,3 +32,88 @@ for i in range(10):
 aptitudes = []
 for individuo in poblacion:
     aptitudes.append(fitnessFuncion(individuo))
+    
+    
+print("\nPOBLACION: ")    
+for i in range(10):
+    print(poblacion[i])
+    
+print("\nAPTITUDES: ")    
+for i in range(10):
+    print(aptitudes[i])    
+    
+# 2.- FRECUENCIA ESPERADA TOTAL
+f = (np.sum(aptitudes))/(len(aptitudes))
+print("\nFRECUENCIA:")
+print(f)
+    
+    
+# 3.- CALCULAR E
+e = []
+for i in range(10):
+    e.append(aptitudes[i]/f)
+    
+print("\nE: ")
+for i in range(10):
+    print(e[i])
+
+# 4._ PARTE ENTERA Y FRACCIONARIA
+partes = []   
+
+for i in range(10): 
+    partes.append(math.modf(e[i]))
+    
+# PARTE ENTERA
+entero = []    
+fraccionaria = []
+for i in range(10):
+    if float(partes[i][1]) >= 1.0:
+        entero.append(i)
+    else:
+        fraccionaria.append(i)
+    
+# SELECCION PARTE FRACCIONARIA
+parteFraccionaria = []
+for i in range(len(fraccionaria)):
+    parteFraccionaria.append(poblacion[fraccionaria[i]])
+
+
+for i in range(len(fraccionaria)):
+    elegido = random.choices(parteFraccionaria, k=1)        
+    
+print("PARTES: ")    
+print(partes)
+
+print("\nPARTE ENTERA, INDICES: ")
+print(entero)
+print("\nPARTE FRACCIONARIA, INDICES: ")
+print(fraccionaria)
+print("\nPARTE FRACCIONARIA") 
+print(parteFraccionaria)
+print("\nELEGIDO")   
+print(elegido)    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
