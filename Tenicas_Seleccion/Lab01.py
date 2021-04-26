c8d41eeb0716908df93dbb3bdee4781b961a402b
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-26 16:05:31 -0500

    cruza por dos puntos

--- Tenicas_Seleccion/Lab01.py
+++ Tenicas_Seleccion/Lab01.py
@@ -23,9 +23,9 @@ class Gen:
     
     def __init__(self):
         self.tamCad = 10
+        self.cromosoma = random.choices(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '], k = 10)
     
-    
-    def Aptitud(individuo):
+    def Aptitud(self,individuo):
         objetivo = ['h', 'o', 'l', 'a', ' ', 'm', 'u', 'n', 'd', 'o']
         aptitud = 0
         indice = 0
@@ -34,8 +34,22 @@ class Gen:
                 aptitud+=1
                 indice +=1
         return aptitud + 1e-4                
-                
-    def cruza(self, otro, aptitud=None):
+                    
+
+
+    def inicializa(self, num):
+        poblacion = []
+        for i in range(int(num)):
+            individuo = random.choices(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '], k = int(self.tamCad))
+            poblacion.append(individuo)
+                        
+        aptitudes = []
+        for individuo in poblacion:
+            aptitudes.append(self.Aptitud(individuo)) 
+        return individuo
+    
+
+    def cruza(self, otro, aptitudd=None):
         padre = self.cromosoma
         madre = otro.cromosoma
         # Crear hijos con cruza por dos puntos
@@ -56,22 +70,87 @@ class Gen:
         h2 = copy.deecopy(otro)
         h2.cromosoma = hijo2
         
-        if aptitud is None:
+        if aptitudd is None:
             return [h1, h2] 
-        aptitudPadre = aptitud(self)
-        aptitudMadre = aptitud(otro)
-        aptitudHijo1 = aptitud(h1)
-        aptitudHijo2 = aptitud(h2)
+        aptitudPadre = aptitudd(self)
+        aptitudMadre = aptitudd(otro)
+        aptitudHijo1 = aptitudd(h1)
+        aptitudHijo2 = aptitudd(h2)
 
         # Generar mejores hijos
         while aptitudHijo1 < aptitudPadre or aptitudHijo1 < aptitudMadre or aptitudHijo2 < aptitudPadre or aptitudHijo2 < aptitudMadre:
-            h1.inicializa(self.tam)
-            h2.inicializa(self.tam)
-            aptitudHijo1 = aptitud(h1)
-            aptitudHijo2 = aptitud(h2)
+            h1.inicializa(self.tamCad)
+            h2.inicializa(self.tamCad)
+            aptitudHijo1 = aptitudd(h1)
+            aptitudHijo2 = aptitudd(h2)
         return [h1,h2]
+ 
+
+    def __str__(self):
+        return "Individuo: "+str(self.individuo)+" ----> Fitness: "+str(self.aptitud)
+        
+        
+    
 class Cromosoma:
-    genes = []
-    gen = Gen() 
-    genes.append(gen)
+ 
+    
+    
+    def Aptitud(self,individuo):
+        objetivo = ['h', 'o', 'l', 'a', ' ', 'm', 'u', 'n', 'd', 'o']
+        aptitud = 0
+        indice = 0
+        for letra in objetivo:
+            if letra == individuo[indice]:
+                aptitud+=1
+                indice +=1
+        return aptitud + 1e-4       
+        
+
+    def inicializa(self, n):
+        poblacion = []
+        for i in range(int(n)):
+            individuo = random.choices(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '], k = int(n))
+            poblacion.append(individuo)
+                        
+        aptitudes = []
+        for individuo in poblacion:
+            aptitudes.append(self.Aptitud(individuo)) 
+    
+        genes = [] 
+        g = Gen()
+        g.inicializa(n)
+        genes.append(g)            
+        self.genes = genes
+        return individuo
+        
+        
+
+    def __str__(self):
+        return "Individuo: "+str(self.inicializa(10))+" ----> Fitness: "+str(self.Aptitud(self.genes))
+ 
+
+    def cruza(self, otro):
+        '''
+        Operador de cruza con otro gen
+
+        :param `otro`: Otro cromosoma con la misma estuctura
+        :returns: Dos hijos
+        :rtype: Cromosma
+        '''
+        h1 = copy.deepcopy(self)
+        h2 = copy.deepcopy(otro)        
+        genesHijos1 = []
+        genesHijos2 = []
+
+        for i in range(len(self.genes)):
+            GenPadre = self.genes[i]
+            GenMadre = otro.genes[i]
+            genHijos = GenPadre.cruza(GenMadre)
+            genesHijos1.append(genHijos[0])
+            genesHijos2.append(genHijos[1])
+        h1.genes = genesHijos1
+        h2.genes = genesHijos2
+        return [h1, h2]
+
+
 
--- Tenicas_Seleccion/TestLab01.py
+++ Tenicas_Seleccion/TestLab01.py
@@ -16,46 +16,43 @@ Created on Mon Mar 22 13:31:09 2021
 
 @author: gustavo
 """
-from Lab01 import Gen, Cromosoma
 import numpy as np
-# El mejor individuo que el número 50
+
+from Lab01 import Cromosoma
 
 def MyFuncion(ind):
     valor = ind.fenotipo()    
     return np.exp(-np.abs(50-valor))
 
-p = Gen()
-m = Gen()
-p.inicializa(0, 100, True)
-m.inicializa(0, 100, True)
+p = Cromosoma()
+m = Cromosoma()
+
+p.inicializa(10)
+m.inicializa(10)
+
+
 print("Padre: ")
+print(p)
 print(p)
 print("Madre: ")
 print(m)
-hijos = p.cruzar(m, MyFuncion)
+print(m)
+
+hijos = m.cruza(p)
+
 print("Hijo 0: ")
 print(hijos[0])
 print("Hijo 1: ")
 print(hijos[1])
 
-print("-----------GEN ENTERO Y REAL---------")
-print("Padre: ")
-p = Cromosoma()
-p.inicializa([1.,0], [10.,8], [True, True])
-print(p)
 
-print("Madre: ")
-m = Cromosoma()
-m.inicializa([1.,0], [10.,8], [True, True])
-print(m)
 
-hijos = p.cruzar(m)
-print("Hijo 0: ")
-print(hijos[0])
-print("Hijo 0: ")
-print(hijos[1])
 
-print("Mutacion: ")
-# Mutaremos al padre
-p.mutar(1)
-print(p)
+
+
+
+
+
+
+
+
