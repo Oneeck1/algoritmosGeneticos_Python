91e0108f25208a8841c04fac19e2c8f1bb0f24a1
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-26 13:58:11 -0500

    aptitud

--- Tenicas_Seleccion/Lab01.py
+++ Tenicas_Seleccion/Lab01.py
@@ -9,8 +9,23 @@ Created on Mon Apr 26 13:44:07 2021
 class Gen:
     
     
+    def Aptitud(individuo):
+        objetivo = ['c','a','f','e']
+        aptitud = 0
+        indice = 0
+        for letra in objetivo:
+            if letra == individuo[indice]:
+                aptitud+=1
+                indice +=1
+        return aptitud + 1e-4 # Sumo para evitar los 0
+
+    def cruza():
+        
     
     
     
 class Cromosoma:
-    
+    genes = []
+    gen = Gen() 
+    genes.append(gen)
+
