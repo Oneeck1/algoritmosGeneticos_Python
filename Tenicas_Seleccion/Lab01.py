1625f733aa2064c24bbe26636524607fa49c61f0
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-26 13:46:52 -0500

    creando archivo para ejercicio 1

---
+++ Tenicas_Seleccion/Lab01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 26 13:44:07 2021
+
+@author: gustavo
+"""
+
+class Gen:
+    
+    
+    
+    
+    
+class Cromosoma:
+    
