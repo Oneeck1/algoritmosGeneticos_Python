a9468c82e9332ff948ac8d8b16e48f5dcf795eb6
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-30 00:50:22 -0500

    inicializando variables

--- Tenicas_Seleccion/Lab01Full.py
+++ Tenicas_Seleccion/Lab01Full.py
@@ -5,12 +5,14 @@ Created on Thu Apr 29 23:55:54 2021
 
 @author: gustavo
 """
+import random
 
 
 class Gen():
     
-    def __init__():
-        pass
+    def __init__(self, tamCad):
+        self.tamCad = tamCad
+        self.cromosoma = random.choices(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '], k = tamCad)
 
     def inicializa():
         pass
