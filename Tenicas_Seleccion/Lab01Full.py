66305d24ea9dbd575c75f95cf0a1f045e20399e2
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-30 00:44:56 -0500

    agregando metodos

---
+++ Tenicas_Seleccion/Lab01Full.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 29 23:55:54 2021
+
+@author: gustavo
+"""
+
+
+class Gen():
+    
+    def __init__():
+        pass
+
+    def inicializa():
+        pass
+    
+    def mutar():
+        pass
+    
+    def cruzar():
+        pass
+    
+    def __str__():
+        pass
+    
+    def fenotipo():
+        pass
+
+class Cromosoma():
+
+    def __init__():
+        pass
+
+    def inicializa():
+        pass
+    
+    def mutar():
+        pass
+    
+    def cruzar():
+        pass
+    
+    def __str__():
+        pass
+    
+    def fenotipo():
+        pass
+
+
+
