5b63c9c15743da75bf2a46dcb4a503a17e6f66d1
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-30 23:15:43 -0500

    Programa Terminado

--- Tenicas_Seleccion/Lab01Full.py
+++ Tenicas_Seleccion/Lab01Full.py
@@ -12,7 +12,7 @@ import copy
 objetivo = ['h', 'o', 'l', 'a', ' ', 'm', 'u', 'n', 'd', 'o'] # Nuestra palabra objetivo 
 
 class Gen():
-    
+    # NOS DICE LAS COINCIDENCIAS EN LA POSICION EXACTA 
     def Aptitud(self,individuo): # Se pedira el individuo     
         aptitud = 0 
         indice = 0
@@ -22,15 +22,15 @@ class Gen():
             indice +=1  # Incrementa el indice en uno para seguir con busca de coincidencias
         return aptitud + 1e-4   # Se pone e-4, para que no sea cero, y hacer operaciones despues
 
-    
+    # NOS MUESTRA EL NUMERO DE CARACTERES IGUALES (SIN IMPORTAR EL ORDEN)
     def Aptitud2(self, individuo):
         aptitud2 = 0
         indice = 0
         for letra in objetivo:
-            if letra in individuo:
+            if letra in individuo: # Verifica si la letra está en todo el individuo, y suma si hay
                 aptitud2 += 1                               
             indice += 1
-        return aptitud2 + 1e-4
+        return aptitud2 + 1e-4  # Se guarda en aptitud, y se pone e-4, para que no haya problemas en los calculos por el cero
     
     def inicializa(self, tamCad, numeroInd): # Se pedirá el tamañoCadena y NumeroDeIndividuos
         self.tamCad = tamCad
@@ -42,7 +42,6 @@ class Gen():
             individuo = self.cromosoma # Se asigna cada individuo a cromosoma
             poblacion.append(individuo) # Se agregan los individuos para obtener la poblacion
     
-    
         aptitudes = [] # Se reserva espacio para las aptitudes
         for individuo in poblacion:
             aptitudes.append(self.Aptitud(individuo)) # Se hace el calculo de cada aptitud en cada individuo
@@ -52,18 +51,19 @@ class Gen():
 
         aptitudes2 = []
         for individuo in poblacion:
-            aptitudes2.append(self.Aptitud2(individuo))
+            aptitudes2.append(self.Aptitud2(individuo)) # Se hace el calculo de cada aptitud2 en cada individui
             
         for i in range(len(poblacion)):
-            self.aptitudes2 = aptitudes2[i]
+            self.aptitudes2 = aptitudes2[i] # Se guarda cada aptitud en aptitudes2
 
     
     def __str__(self):
         # Se imprime el resultado en pantalla de los individuos y su fitnes respectivo
-        return "Ind: "+str(self.cromosoma)+" -> Fitness: "+str(self.aptitudes)+", Fitness2: "+str(self.aptitudes2)
+        crom = self.cromosoma
+        crom = str(crom[1:]).replace('[', '').replace(']', '').replace(',', '').replace("'","")
+        return "Individuo:   "+str(crom)+" ---> Fitness: "+str(self.aptitudes)+", Fitness2: "+str(self.aptitudes2)
     
-    def Mutar(self, n):
-        pass
+
     
     def Cruzar(self, otro, fitnessFunction=None):
          # Creación de los padres
@@ -84,24 +84,24 @@ class Gen():
         h1 = copy.deepcopy(self) # Clona el objeto actual
         h1.cromosoma = hijo1
         
-        h2 = copy.deepcopy(otro)
+        h2 = copy.deepcopy(otro) # Clona el nuevo objeto
         h2.cromosoma = hijo2
         
         if fitnessFunction is None:
-            return [h1,h2]
+            return [h1,h2]  # Se retornan los hijos
         aptitudPadre = fitnessFunction(self)
         aptitudMadre = fitnessFunction(otro)
         aptitudHijo1 = fitnessFunction(h1)
         aptitudHijo2 = fitnessFunction(h2)
         
         while aptitudHijo1 < aptitudMadre or aptitudHijo1 < aptitudPadre or aptitudHijo2 < aptitudMadre or aptitudHijo2 < aptitudPadre:    
-            h1 = Cromosoma()
-            h1.inicializa(self.tamCad,self.numeroInd)
-            h2 = Cromosoma()
-            h2.inicializa(self.tamCad, self.numeroInd)
+            h1 = Cromosoma()    # Se crea un objeto de tipo Cromosoma
+            h1.inicializa(self.tamCad,self.numeroInd) # Se inicializas
+            h2 = Cromosoma()    # Se crea un objeto de tipo Cromosoma
+            h2.inicializa(self.tamCad, self.numeroInd) # Se inicializa
             aptitudHijo1 = fitnessFunction(h1)                
             aptitudHijo2 = fitnessFunction(h2)                
-        return [h1,h2]
+        return [h1,h2] # Se retornan los hijos
 
 class Cromosoma():
     
