1e7739aefd8748802b062576ba66a83c1005df6f
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-03 13:59:25 -0500

    Area Triangulo

---
+++ AreaTriangulo/area.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  3 13:23:13 2021
+
+UNIVERSIDAD AUTÓNOMA DEL ESTADO DE MÉXICO
+CENTRO UNIVERSITARIO UAEM ZUMPANGO
+INGENIERÍA EN COMPUTACIÓN
+ALGORITMOS GENÉTICOS 2021
+TEMA: Representación de soluciones numéricas para AG con POO
+ALUMNO: GUSTAVO RODRIGUEZ CALZADA
+PROFESOR: ASDRÚBAL LÓPEZ CHAU
+
+DESCRIPCIÓN: Area Máxima de un triangulo
+
+@author: gustavo
+"""
+
+
