a79ec3de20b13fa64aed2eb4d6d2b3679f18b9f7
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-17 14:00:42 -0500

    area maxima de un triangulo AG

--- AG_aplicado/Cromosomas.py
+++ AG_aplicado/Cromosomas.py
@@ -7,26 +7,25 @@ CENTRO UNIVERSITARIO UAEM ZUMPANGO
 INGENIERÍA EN COMPUTACIÓN
 ALGORITMOS GENÉTICOS 2021
 TEMA: Representación de soluciones numéricas para AG con POO
-ALUMNO: GUSTAVO RODRIGUEZ CALZADA
+ALUMNO: tu nombre
 PROFESOR: ASDRÚBAL LÓPEZ CHAU
 
 DESCRIPCIÓN: Super clase de todas las cromosomas numéricas
 
 Created on Mon Mar  8 16:49:47 2021
 
-@author: gustavo
+@author: asdruballopezchau
 """
 import numpy as np
 import random
 import copy
-import math
 
 
 class GenNum:
 
     '''
     La super clase GenNum representa genes que son parte de
-    soluciones numéricas (cromosomas) en un Algoritmo genético. 
+    soluciones numéricas (cromosomas) en un Algoritmo genético.
     TODO: Implementa todos los métodos que sean posibles
     '''
 
@@ -34,7 +33,6 @@ class GenNum:
         pass
 
     def inicializa(self, vMin=0, vMax=15):
-
         '''
         Inicializa de manera pseudo aleatoria al inidividuo.
 
@@ -45,7 +43,6 @@ class GenNum:
         self.vMax = vMax
 
     def isFactible(self):
-
         '''
         Verifica si el gen está dentro de los rangos vMin y vMax
 
@@ -55,14 +52,12 @@ class GenNum:
         return True
 
     def mutar(self, nbits):
-
         '''
-        Aplica mutación al gen        
+        Aplica mutación al gen
         '''
         pass
 
     def cruzar(self, otro):
-        
         '''
         Operador de cruza por dos puntos
 
@@ -80,7 +75,7 @@ class GenNum:
         :rtype: str
         '''
         pass
-    
+
     def fenotipo():
         '''
 
@@ -96,37 +91,35 @@ class GenEntero(GenNum):
     Sub clase de GenNum representa genes que son parte de
     soluciones numéricas enteras en un Algoritmo genético.
     La cadena de bits del gen está binario o gray.
-    
-    TODO: SOBRE ESCRIBE LOS MÉTODOS, APROVECHA LO QUE 
+
+    TODO: SOBRE ESCRIBE LOS MÉTODOS, APROVECHA LO QUE
     PUEDAS DE LA SUPERCLASE
     '''
 
     def inicializa(self, vMin=0, vMax=15, gray=True):
-        
         '''
         Inicializa de manera pseudo aleatoria al inidividuo.
 
         :param `vMin`: El valor mínimo  a representar
         :param `vMax`: El valor máximo  a representar
-        :param `gray`: Valor para indicar si el gen 
+        :param `gray`: Valor para indicar si el gen
                     representa valores en Gray o en Binario
         '''
         super().inicializa(vMin, vMax)
         self.gray = gray
-        # Calculo del número mínimo de bits para representar un valor entre
-        # vMin y vMax
+        #  Cálculo del número minimo de bits para representar un valor
+        #  entre vMin y vMax
         v = max([np.abs(vMin), np.abs(vMax)])
         self.nbits = int(np.ceil(np.log(v + 1)/np.log(2)) + 1)
-        self.cromosoma = random.choices([0, 1], k = self.nbits)
-        # Generar un individuo factible
+        self.cromosoma = random.choices([0, 1], k=self.nbits)
+        # Generar un indivividuo factible
         while not self.isFactible():
-            self.cromosoma = random.choices([0, 1], k = self.nbits)
+            self.cromosoma = random.choices([0, 1], k=self.nbits)
 
     # Regresa el fenotipo: El valor que representa el cromosoma
     def fenotipo(self):
-        if not self.gray :  #  Representación en binario
+        if not self.gray:  # Representación en binario
             cad = str(self.cromosoma[1:]).replace('[', '').replace(']', '').replace(',', '').replace(' ', '')
-
         else:  # Representación en Gray
             binario = self.cromosoma.copy()
             for i in range(2, len(self.cromosoma)):
@@ -136,10 +129,10 @@ class GenEntero(GenNum):
                     binario[i] = 0
                 else:
                     binario[i] = 1
-            cad = str(binario[1:]).replace('[','').replace(']','').replace(',','').replace(' ','')
-        if self.cromosoma[0] == 0: 
+            cad = str(binario[1:]).replace('[', '').replace(']', '').replace(',', '').replace(' ', '')
+        if self.cromosoma[0] == 0:
             return int(cad, 2)
-        else: 
+        else:
             return -int(cad, 2)
 
     #  Regresa True si el individuo representa una solucion factible, y False en otro caso
@@ -157,7 +150,7 @@ class GenEntero(GenNum):
             if self.isFactible():
                 break
 
-    def cruzar(self, otro, FuncionAptitud=None):
+    def cruzar(self, otro, funcionAptitud=None):
         padre = self.cromosoma
         madre = otro.cromosoma
         # Crear hijos con cruza por dos puntos
@@ -167,39 +160,38 @@ class GenEntero(GenNum):
         hijo2 = madre.copy()
         medio1 = madre[cp1:cp2]
         medio2 = padre[cp1:cp2]
-        # Extremos del padre y centro de la madre
-        hijo1[cp1:cp2] = medio1        
-        # Extremos de la madre y centro del padre
+        hijo1[cp1:cp2] = medio1
         hijo2[cp1:cp2] = medio2
-        # Crea una copia exacta
-        h1 = copy.deepcopy(self)
+        h1 = copy.deepcopy(self)  # Crea una copia exacta
         h1.cromosoma = hijo1
-        # Clonado de un objeto, incluyendo metodos y funciones, etc
         h2 = copy.deepcopy(otro)
         h2.cromosoma = hijo2
-        
-        if FuncionAptitud is None:
+        if funcionAptitud is None:
             return [h1, h2]
-        aptitudPadre = FuncionAptitud(self)
-        aptitudMadre = FuncionAptitud(otro)
-        aptitudHijo1 = FuncionAptitud(h1)
-        aptitudHijo2 = FuncionAptitud(h2)            
+        aptitudPadre = funcionAptitud(self)
+        aptitudMadre = funcionAptitud(otro)
+        aptitudHijo1 = funcionAptitud(h1)
+        aptitudHijo2 = funcionAptitud(h2)
 
-        # Genera hijos mejores siempre
-        while aptitudHijo1 < aptitudPadre or aptitudHijo1 < aptitudMadre or aptitudHijo2 < aptitudPadre or aptitudHijo2 < aptitudMadre:
+        # Genera hijos mejores siempre.
+        while aptitudHijo1 < aptitudPadre \
+            or aptitudHijo1 < aptitudMadre \
+        or aptitudHijo2 < aptitudPadre \
+        or aptitudHijo2 < aptitudMadre:
             h1 = GenEntero()
             h1.inicializa(self.vMin, self.vMax)
             h2 = GenEntero()
             h2.inicializa(otro.vMin, otro.vMax)
-            return [h1, h2]
+            aptitudHijo1 = funcionAptitud(h1)
+            aptitudHijo2 = funcionAptitud(h2)
+        return [h1, h2]
 
     def __str__(self):
-        return str(self.cromosoma) +  " (" + str(self.fenotipo()) + ")"
-        
+        return str(self.cromosoma) + " (" + str(self.fenotipo()) + ")"
+
 
 class GenReal(GenNum):
 
-    
     '''
     Sub clase de GenNum representa genes que son parte de
     soluciones numéricas reales en un Algoritmo genético.
@@ -210,7 +202,6 @@ class GenReal(GenNum):
     '''
 
     def inicializa(self, vMin=0, vMax=15, gray=True):
-
         '''
         Inicializa de manera pseudo aleatoria al inidividuo.
 
@@ -219,177 +210,7 @@ class GenReal(GenNum):
         :param `gray`: Valor para indicar si el gen 
                     representa valores en Gray o en Binario
         '''
-
-            
-        super().inicializa(vMin, vMax)
-        self.gray = gray
-        # Calculo del número mínimo de bits para representar un valor entre
-        # vMin y vMax
-        v = max([vMin, vMax])  
-        
-        self.nbits = int(np.ceil(np.log(v + 1)/np.log(2)) + 1) # Saber cuantos bits ocupo para parte entera
-        global entero
-        global decimal
-        global cadena
-        
-        # Numeros aleatorios punto flotante
-        #NumAl = random.uniform (vMin, vMax)
-        # print(NumAl)
-                                                
-#        self.cromosoma = random.choices([0, 1], k = self.nbits)
-        entero = random.choices([0, 1], k = self.nbits)
-        decimal = random.choices([0, 1], k = self.nbits)
-      
-        #self.cromosoma = entero,decimal
-        # self.entero_decimal(NumAl)
-        entero = str(entero[1:]).replace('[', '').replace(']', '').replace(',', '').replace(' ', '')
-        #entero22 = str(entero[1:]).replace('[', '').replace(']', '').replace(',', '').replace(' ', '')
-        decimal = str(decimal[1:]).replace('[', '').replace(']', '').replace(',', '').replace(' ', '')
-
-        
-        cadena = str(entero)+"."+str(decimal)
-        self.cromosoma = list(cadena)            
-        
-       
-        # Generar un individuo factible
-        while not self.isFactible():
-            # self.cromosoma = random.choices([0, 1], k = self.nbits)
-            # self.entero_decimal(NumAl)
-            # self.entero_decimal(NumAl)
-            entero = random.choices([0, 1], k = self.nbits)
-            decimal = random.choices([0, 1], k = self.nbits)
-            entero = str(entero[1:]).replace('[', '').replace(']', '').replace(',', '').replace(' ', '').replace("'","")
-         #   entero22 = str(entero[1:]).replace('[', '').replace(']', '').replace(',', '').replace(' ', '')
-            decimal = str(decimal[1:]).replace('[', '').replace(']', '').replace(',', '').replace(' ', '').replace("'","")
-
-            cadena = str(entero)+"."+str(decimal)
-            # cadena = str(cadena[:].replace("'",""))
-            self.cromosoma = list(cadena)  
-            #self.cromosoma = entero,decimal
-     
-           
-#           numC = str(entero)+"."+str(decimal)
-
-# Regresa el fenotipo: El valor que representa el cromosoma
-    def fenotipo(self):
-
-        global entero2
-        global decimal2         
-        if not self.gray :  #  Representación en binario
-            cro = str(entero)+"."+str(decimal)
-            cad = str(cro[1:]).replace('[', '').replace(']', '').replace(',', '').replace(' ', '').replace("'","")
-            
-        else:  # Representación en Gray
-            entero2 = str(entero[1:]).replace('[', '').replace(']', '').replace(',', '').replace(' ', '')
-        #entero22 = str(entero[1:]).replace('[', '').replace(']', '').replace(',', '').replace(' ', '')
-            decimal2 = str(decimal[1:]).replace('[', '').replace(']', '').replace(',', '').replace(' ', '')
-
-            binario = self.cromosoma.copy()
-            binario2 = self.cromosoma.copy()
-            # PARTE ENTERA
-            for i in range(2,len(entero2)):
-                a = entero2[i-1]
-                b = entero2[i]
-
-                if a == b:
-                    binario[i] = 0
-                    
-                elif a == '.' or b == '.': # Si tiene un . que lo pase
-                    pass
-                else:
-                    binario[i] = 1                                
-            cade = str(binario[1:]).replace('[','').replace(']','').replace(',','').replace(' ','').replace("'","")
-            # PARTE DECIMAL
-            for i in range(2, len(decimal2)):
-                aa = decimal2[i-1]
-                bb = decimal2[i]
-                if aa == bb:
-                    binario2[i] = 0
-                elif  aa == '.' or bb == '.':
-                    pass
-                else:
-                    binario2[i] = 1
-            #cade2 = str(binario2[1:]).replace('[','').replace(']','').replace(',','').replace(' ','').replace("'","")
-            
-          #  cadenaFull = str(cade)+"."+str(cade2)
-        #entero22 = str(entero[1:]).replace('[', '').replace(']', '').replace(',', '').replace(' ', '')
-        #return int(entero2,2)
-            
-        if(entero2[0]==0):
-            c = int(entero2,2)
-            C1 = int(decimal2,2)
-            cad1 = str(c)+"."+str(C1)
-            return float(cad1)
-        else:
-            c2 = int(entero2,2)
-            C22 = int(decimal2,2)
-            cad2 = str(c2)+"."+str(C22)
-            return -float(cad2)
-        
-        
-    #  Regresa True si el individuo representa una solucion factible, y False en otro caso
-    def isFactible(self):
-        if self.fenotipo() >= self.vMin and self.fenotipo() <= self.vMax:
-            return True
-        else:
-            return False
-
-    def mutar(self, nbits):
-        while True:
-            #bits_cambiar = random.choices(range(len(decimal2)), k=nbits)
-            #y = random.choices(range(1,4))
-            #bits_cambiar = decimal2[y]
-            #caden = str(cadena[:].replace("'",""))
-            # self.cromosoma = self.cromosoma.replace('0','1',1)   
-            if self.cromosoma[1] == '1':
-                self.cromosoma[1] = '0'
-            else:
-                self.cromosoma[1] = '1'
-            
-            if self.isFactible():
-                break
-
-    def cruzar(self, otro, FuncionAptitud=None):
-        padre = self.cromosoma
-        madre = otro.cromosoma
-        # Crear hijos con cruza por dos puntos
-        cp1 = int(np.ceil(len(entero2)/3))
-        cp2 = int(2*cp1)
-        hijo1 = padre.copy()
-        hijo2 = madre.copy()
-        medio1 = madre[cp1:cp2]
-        medio2 = padre[cp1:cp2]
-        # Extremos del padre y centro de la madre
-        hijo1[cp1:cp2] = medio1        
-        # Extremos de la madre y centro del padre
-        hijo2[cp1:cp2] = medio2
-        # Crea una copia exacta
-        h1 = copy.deepcopy(self)
-        h1.cromosoma = hijo1
-        # Clonado de un objeto, incluyendo metodos y funciones, etc
-        h2 = copy.deepcopy(otro)
-        h2.cromosoma = hijo2
-        
-        if FuncionAptitud is None:
-            return [h1, h2]
-        aptitudPadre = FuncionAptitud(self)
-        aptitudMadre = FuncionAptitud(otro)
-        aptitudHijo1 = FuncionAptitud(h1)
-        aptitudHijo2 = FuncionAptitud(h2)            
-
-        # Genera hijos mejores siempre
-        while aptitudHijo1 < aptitudPadre or aptitudHijo1 < aptitudMadre or aptitudHijo2 < aptitudPadre or aptitudHijo2 < aptitudMadre:
-            h1 = GenEntero()
-            h1.inicializa(self.vMin, self.vMax)
-            h2 = GenEntero()
-            h2.inicializa(otro.vMin, otro.vMax)
-            return [h1, h2]
-
-    def __str__(self):
-        palabra = str(self.cromosoma) + " (" + str(self.fenotipo()) + ")" + "     <-------->REAL"
-        palabra = str(palabra[:].replace("'",""))
-        return palabra
-
+        pass
 
 
 class Cromosoma:
@@ -403,15 +224,13 @@ class Cromosoma:
     TODO: LA CLASE CROMOSOMA TIENE GENES" APROVECHA LO QUE 
     PUEDAS DE LAS CLASES GenNum, GenEntero y GenReal.
     '''
-    pass
 
     def __init__(self):
-        
         '''
         Forma un cromosoma con los genes del inidividuo en la lista.
 
         :param `listaGenes`: Una lista con genes (subtipos de GenNum).
-        '''        
+        '''
         pass
 
     def inicializa(self, vMins, vMaxs, grays):
@@ -431,10 +250,7 @@ class Cromosoma:
         for i in range(len(vMins)):
             if type(vMins[i]) is float or type(vMaxs[i]) is float:
                 # Genes representación real
-                g = GenReal()
-                g.inicializa(vMins[i], vMaxs[i], gray=grays[i])
-                genes.append(g)
-            
+                pass
             else:
                 #  Representación entera
                 g = GenEntero()
@@ -457,7 +273,6 @@ class Cromosoma:
         for gen in self.genes:
             gen.mutar(nbits)
 
-
     def cruzar(self, otro):
         '''
         Operador de cruza con otro gen
@@ -467,25 +282,22 @@ class Cromosoma:
         :rtype: Cromosma
         '''
         h1 = copy.deepcopy(self)
-        h2 = copy.deepcopy(otro)        
+        h2 = copy.deepcopy(otro)
         genesHijos1 = []
         genesHijos2 = []
-
         for i in range(len(self.genes)):
-            GenPadre = self.genes[i]
-            GenMadre = otro.genes[i]
-            genHijos = GenPadre.cruzar(GenMadre)
+            genPadre = self.genes[i]
+            genMadre = otro.genes[i]
+            genHijos = genPadre.cruzar(genMadre)
             genesHijos1.append(genHijos[0])
             genesHijos2.append(genHijos[1])
         h1.genes = genesHijos1
         h2.genes = genesHijos2
         return [h1, h2]
-
-    # LABORATORIO IMPLEMENTAR LA CRUZA CON FUNCION DE AOTITUD
-    # PARA PRODUCIR SIEMPRE MEJORES HIJOS
+        # LABORATORIO: Implementar la cruza con función de aptitud
+        # para producir siempre mejores hijos.
 
     def __str__(self):
-        
         '''
         Imprime como una cadena el cromosoma.
 
