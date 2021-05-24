ad9fac1359318c69204def7c33e6f928431456e7
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-24 15:06:31 -0500

    AG Clustering

---
+++ Cluster/clustering.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 24 15:04:45 2021
+
+@author: gustavo
+"""
+
+
+class IndividuoCluster:
+    def __init__(self):
+        pass
+    def inicializa(self, K):
+        pass
+    def cruzar(self, madre):
+        pass
+    def mutar(self):
+        pass
+    def print(self):
+        pass
